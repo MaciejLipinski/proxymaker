353c554fab35682af5ab2eade9efed38a79dcdb6
MaciejLipinski <lipinski.mt@gmail.com>
2024-03-15 19:20:47 +0100

    fix distribution

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    application
     kotlin("jvm") version "1.9.21"
 }
 
@@ -13,6 +14,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.21")
     implementation("org.seleniumhq.selenium:selenium-java:4.18.1")
     implementation("org.apache.pdfbox:pdfbox:3.0.1")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
@@ -26,4 +28,8 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
+}
+
+application {
+    mainClass.set("proxymaker.Main")
 }
--- src/main/kotlin/proxymaker/Main.kt
+++ src/main/kotlin/proxymaker/Main.kt
@@ -4,43 +4,77 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import java.io.File
+import java.io.FileWriter
+import java.nio.file.Paths
+import kotlin.system.exitProcess
 
 private const val DEFAULT_PROPERTIES_PATH = "src/main/resources/properties.yaml"
 
-fun main() {
-    var propertiesPath = DEFAULT_PROPERTIES_PATH
-    if (!File(propertiesPath).exists() || !File(propertiesPath).canRead()) {
-        propertiesPath = askForInput("Enter properties.yaml path:")
-    }
-    val properties =
-        ObjectMapper(YAMLFactory())
-            .registerKotlinModule()
-            .readValue(File(propertiesPath), Properties::class.java)
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(vararg args: String) {
+            if (!File(propertiesPath).exists() || !File(propertiesPath).canRead()) {
+                createPropertiesFile()
+                println("Edit properties.yaml before using")
+                exitProcess(0)
+            }
+            val properties =
+                ObjectMapper(YAMLFactory())
+                    .registerKotlinModule()
+                    .readValue(File(propertiesPath), Properties::class.java)
 
-    val mode = askForInput(
-        prompt = "Choose mode: pngmaker|pdfmaker",
-        allowedInputs = listOf("pngmaker", "pdfmaker")
-    ).let { Mode.fromString(it) }
+            val mode = askForInput(
+                prompt = "Choose mode: pngmaker|pdfmaker",
+                allowedInputs = listOf("pngmaker", "pdfmaker")
+            ).let { Mode.fromString(it) }
 
-    when (mode) {
-        Mode.PngMaker -> PngMaker.makeProxyImages(properties.pngMaker)
-        Mode.PdfMaker -> makePdf(properties.pdfMaker)
-    }
-}
+            when (mode) {
+                Mode.PngMaker -> PngMaker.makeProxyImages(properties.pngMaker)
+                Mode.PdfMaker -> makePdf(properties.pdfMaker)
+            }
+        }
 
-private fun makePdf(properties: PdfMakerProperties) = PdfMaker.makePdf(
-    decksDir = properties.decksDir ?: askForInput("Enter images directory:"),
-    imageExtension = properties.imageExtension,
-    borderColor = properties.borderColor,
-    fileName = properties.fileName,
-)
+        private val propertiesPath: String
+            get() {
+                val currentDir = Paths.get("").toAbsolutePath().toString()
+                return "$currentDir/properties.yaml"
+            }
 
-private fun askForInput(prompt: String, allowedInputs: List<String>? = null): String {
-    println(prompt)
-    while (true) {
-        val input = readLine()
-        if (!input.isNullOrEmpty() && allowedInputs?.contains(input) != false) {
-            return input
+        private fun createPropertiesFile() {
+            val propertiesFile = File(propertiesPath)
+            FileWriter(propertiesFile).use {
+                it.write(
+                    """
+                |pngMaker:
+                |  oracleUrl: https://premodernoracle.com
+                |  decklist: decklist.txt
+                |#  decksDir: # Will save to user's home directory when not set
+                |pdfMaker:
+                |  decksDir: some/dir
+                |  imageExtension: png
+                |  borderColor: BLACK # allowed values: BLACK, WHITE, GOLD, SILVER
+                |  fileName: proxies.pdf
+                |""".trimMargin()
+                )
+            }
+        }
+
+        private fun makePdf(properties: PdfMakerProperties) = PdfMaker.makePdf(
+            decksDir = properties.decksDir ?: askForInput("Enter images directory:"),
+            imageExtension = properties.imageExtension,
+            borderColor = properties.borderColor,
+            fileName = properties.fileName,
+        )
+
+        private fun askForInput(prompt: String, allowedInputs: List<String>? = null): String {
+            println(prompt)
+            while (true) {
+                val input = readLine()
+                if (!input.isNullOrEmpty() && allowedInputs?.contains(input) != false) {
+                    return input
+                }
+            }
         }
     }
 }
